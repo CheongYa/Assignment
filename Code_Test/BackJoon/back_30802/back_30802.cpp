8df1acf699b9eaf984780334194c6a7d35bcb3dd
CheongYa <chch1234569@naver.com>
2024-06-23 20:25:10 +0900

    Upload back_30802

---
+++ Code_Test/BackJoon/back_30802/back_30802.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N, T, P;
+int tSize[6];
+int tCnt = 0;
+
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < 6; i++)
+        cin >> tSize[i];
+    cin >> T >> P;
+
+    for (int i = 0; i < 6; i++)
+        tCnt += (tSize[i] / T + (tSize[i] % T > 0));
+
+    cout << tCnt << '\n';
+    cout << N / P << ' ' << N % P;
+
+    return 0;
+}
+
+
